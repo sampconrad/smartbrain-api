0c8573e35549a0ada797bb0a0561fc8e6efe5169
sampconrad <sampconrad@gmail.com>
2022-05-18 14:49:44 -0300

    adding port

--- server.js
+++ server.js
@@ -52,7 +52,4 @@ app.post('/imageurl', (req, res) => {
 	image.handleApiCall(req, res);
 });
 
-const PORT = process.env.PORT;
-app.listen(PORT || 3000, () => {
-	console.log(`app is running on port ${PORT}`);
-});
+app.listen(process.env.PORT || 3000);
